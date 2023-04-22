5cd4172c71d85e0e7929a81c6206efd0a24a2470
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-04-22 23:26:30 +0500

    Новые экраны и анимация

--- app/src/main/java/com/example/rainbowgoldstation/MainActivity2.kt
+++ app/src/main/java/com/example/rainbowgoldstation/MainActivity2.kt
@@ -1,11 +1,35 @@
 package com.example.rainbowgoldstation
 
+import android.content.Intent
+import android.icu.util.TimeUnit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.example.rainbowgoldstation.databinding.ActivityMain2Binding
+import java.util.concurrent.Executors
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonStart.setOnClickListener(){
+            intent = Intent(this@MainActivity2,MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+        val myAnimation = AnimationUtils.loadAnimation(this,R.anim.scale)
+        binding.imageViewBack.setOnClickListener(){
+            binding.imageViewBack.startAnimation(myAnimation)
+        }
+
+
+
     }
+
+
+
 }
